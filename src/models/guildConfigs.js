569207ca5e964330ab25e2f1d21bd2d79ca56726
Exxon <65795854+Exxonnnnnn@users.noreply.github.com>
2021-04-04 18:57:05 +0100

    Remove _id

--- src/models/guildConfigs.js
+++ src/models/guildConfigs.js
@@ -1,20 +1,9 @@
-const mongoose = require('mongoose')
+const { Schema, model } = require('mongoose');
 
-const guildConfigs = new mongoose.Schema({
-    _id: {
-        type: String,
-        required: true,
-    },
+const guildConfigs = new Schema({
+    gId: { type: String, required: true },
+    prefix: { type: String, required: true },
+    premium: { type: String, required: false }
+});
 
-    prefix: {
-        type: String,
-        required: true
-    },
-
-    premium: {
-        type: Boolean,
-        default: false,
-    }
-})
-
-module.exports = mongoose.model('guild-configs', guildConfigs)
+module.exports = model('guild-configs', guildConfigs);
